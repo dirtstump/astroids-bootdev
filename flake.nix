319729313c8c71ec3064d163eb0ba977eef565bc
dirt <dirtstump@gmail.com>
2025-06-09 11:17:59 -0500

    adding flake.nix for dev environment

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-linux = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+    in pkgs.mkShell {
+      packages = with pkgs; [ (python311.withPackages (ps: [ ps.pygame ])) ];
+    };
+  };
+}
