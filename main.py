bc7df00b7d0131045f8fc42bf0c98904d0bee567
dirt <dirtstump@gmail.com>
2025-06-09 19:34:06 -0500

    added asteroids to asteroids

--- main.py
+++ main.py
@@ -5,29 +5,40 @@ import pygame
 
 # import game constants
 from constants import (
-    SCREEN_WIDTH,
+    #    ASTEROID_KINDS,
+    #    ASTEROID_MAX_RADIUS,
+    #    ASTEROID_MIN_RADIUS,
+    #    ASTEROID_SPAWN_RATE,
     SCREEN_HEIGHT,
-    ASTEROID_MIN_RADIUS,
-    ASTEROID_KINDS,
-    ASTEROID_SPAWN_RATE,
-    ASTEROID_MAX_RADIUS,
-    PLAYER_RADIUS,
+    SCREEN_WIDTH,
 )
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
-from player import (
-    Player,
-)
-    
+
 def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+
+    # define groups for organization
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
     dt = 0
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroidfield = AsteroidField()
 
     while True:
         for event in pygame.event.get():
@@ -35,15 +46,17 @@ def main():
                 return
 
         # update gamestate
-        player.update(dt)
+        updatable.update(dt)
 
         # draw gamestate
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
 
         # limit framerate to 60 fps
-        dt = clock.tick(60)/1000
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
